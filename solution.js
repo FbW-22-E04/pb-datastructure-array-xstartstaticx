986f047c22d9a4eaaea746033145a5d22d3b2811
xstartstaticx <daniel.reiller@icloud.com>
2022-07-07 11:49:46 +0200

    finished exercise

---
+++ solution.js
@@ -0,0 +1,11 @@
+// 1
+const myArray = ["1st", "2nd", "3rd", "4th", "5th"];
+console.log(myArray);
+
+// 2
+const thirdElement = myArray[2];
+console.log(thirdElement);
+
+// 3
+myArray.splice(3, 1, "fourth element");
+console.log(myArray);
